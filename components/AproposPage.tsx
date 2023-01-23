0ac8d4dac9f96e432c2cf9314cf09a5a2e2bebea
montei_j <montei_j@etna-alternance.net>
2023-01-23 16:36:16 +0100

    [UPDATE] In AproposPage, text will not depass view

--- components/AproposPage.tsx
+++ components/AproposPage.tsx
@@ -11,7 +11,7 @@ export default function AproposPage() {
           </Flex>
           <Flex w={'100%'} h={'100%'} pt={5}>
             <Flex>
-              <Text fontSize={'17px'} fontFamily='Cutive' w={'700px'}>
+              <Text fontSize={'17px'} fontFamily='Cutive' maxW={'700px'}>
                 {'Bonjour, je m\'appelle João Pedro. Je suis étudiant à l\'école des technologies numériques avancées depuis septembre 2022. J\'ai commencé a apprendre l\'informatique lors de mes années a l\'université avec beaucoup de Java et de C. Aujourd\'hui je fait plus du développement web mais les deux genres de programmations m\'interresent.'}
               </Text>
             </Flex>       
